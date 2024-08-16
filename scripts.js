104fcf32d5ad858dbe684e588d53c3df2297e322
EnfoqueNutricional <elinutricion.info@gmail.com>
2024-08-16 16:21:30 -0500

    quinto

--- scripts.js
+++ scripts.js
@@ -1,23 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.getElementById("slider");
-    const sliderItems = slider.children.length;
-    const itemWidth = slider.querySelector("li").offsetWidth;
-    let currentIndex = 0;
-
-    document.getElementById("prev").addEventListener("click", function() {
-        if (currentIndex > 0) {
-            currentIndex--;
-            slider.style.marginLeft = `-${itemWidth * currentIndex}px`;
-        }
+document.addEventListener('DOMContentLoaded', function () {
+    const swiperEl = document.querySelector('.swiper-container');
+    const swiper = new Swiper(swiperEl, {
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 10,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      initialSlide: 0,
+      autoplay: {
+        delay: 3500, // Tiempo en milisegundos entre cada cambio de slide
+        disableOnInteraction: false, // Continuar la reproducción automática después de la interacción del usuario
+      },
     });
-
-    document.getElementById("next").addEventListener("click", function() {
-        if (currentIndex < sliderItems - 1) {
-            currentIndex++;
-            slider.style.marginLeft = `-${itemWidth * currentIndex}px`;
-        }
+  
+    document.querySelector('.slide-1')?.addEventListener('click', function (e) {
+      e.preventDefault();
+      swiper.slideTo(0, 0);
     });
-});
+  
+    document.querySelector('.slide-250')?.addEventListener('click', function (e) {
+      e.preventDefault();
+      swiper.slideTo(249, 0);
+    });
+  
+    document.querySelector('.slide-500')?.addEventListener('click', function (e) {
+      e.preventDefault();
+      swiper.slideTo(499, 0);
+    });
+  
+    let prependNumber = 2; // Define a number to track prepends
+    document.querySelector('.prepend-2-slides')?.addEventListener('click', function (e) {
+      e.preventDefault();
+      swiper.virtual.prependSlide([
+        'Slide ' + --prependNumber,
+        'Slide ' + --prependNumber,
+      ]);
+    });
+  
+    let appendNumber = 1; // Define a number to track appends
+    document.querySelector('.append-slide')?.addEventListener('click', function (e) {
+      e.preventDefault();
+      swiper.virtual.appendSlide('Slide ' + ++appendNumber);
+    });
+  });
+  
+  
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -30,3 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
         navContainer.classList.toggle('active');
     });
 });
+
+
+
+
