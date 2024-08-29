1583980b1bec4fbbed0fc276f83634a31b19a420
EnfoqueNutricional <elinutricion.info@gmail.com>
2024-08-28 22:06:20 -0500

    Mensaj

--- scripts.js
+++ scripts.js
@@ -77,59 +77,51 @@ document.addEventListener('DOMContentLoaded', function () {
   
 
 
-document.getElementById('consultaForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    // Obtener la consulta ingresada
-    var consulta = document.getElementById('consulta').value.trim();
-    
-    // Asegúrate de reemplazar el número de teléfono con tu número de WhatsApp
-    var telefono = ''; // Reemplaza con tu número en formato internacional
-    
-    if (consulta) {
-        // Construir la URL de WhatsApp Web
-        var url = 'https://web.whatsapp.com/send?phone=' + telefono + '&text=' + encodeURIComponent(consulta);
-        
-        // Redirigir a la URL de WhatsApp Web
-        window.open(url, '_blank');
-    } else {
-        alert('Por favor, escribe tu consulta.');
-    }
-});
 
 
 
-function myFunc() {
-  var now = new Date();
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
-  var seconds = now.getSeconds();
 
-  // Formato de 12 horas
-  var period = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // La hora '0' debe mostrarse como '12'
 
-  // Asegurar que minutos y segundos tengan dos dígitos
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
 
-  // Construir la cadena de tiempo
-  var time = hours + ":" + minutes + ":" + seconds + " " + period;
 
-  // Mostrar el tiempo en el elemento con id 'display-time'
-  document.getElementById('display-time').innerHTML = time;
-}
 
-// Llamar a la función inmediatamente y luego cada segundo
-myFunc();
-setInterval(myFunc, 1000);
+  const $changeTimeMonthly = document.querySelector("#changeTime__monthly");
+const $changeTimeYearly = document.querySelector("#changeTime__yearly");
 
-window.onscroll = function() {
-  var backToTop = document.querySelector('.back-to-top');
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      backToTop.style.display = 'block';
-  } else {
-      backToTop.style.display = 'none';
-  }
-};
+
+const $cardPricePro = document.querySelector("#cardPricePro");
+const $cardPriceEnterprise = document.querySelector("#cardPriceEnterprise");
+const $cardPriceTime1 = document.querySelector(".cardPrice__time--first");
+const $cardPriceTime2 = document.querySelector(".cardPrice__time--second");
+const $cardPriceTime3 = document.querySelector(".cardPrice__time--third");
+
+
+$changeTimeYearly.addEventListener("click" , () => {
+    $cardPriceTime1.textContent = "year";
+    $cardPriceTime2.textContent = "year";
+    $cardPriceTime3.textContent = "year";
+
+    $cardPricePro.textContent = "$99";
+    $cardPriceEnterprise.textContent = "$699";
+
+    $changeTimeYearly.style.backgroundColor = "#4927ec";
+    $changeTimeYearly.style.color = "#fff";
+    $changeTimeMonthly.style.backgroundColor = "transparent";
+    $changeTimeMonthly.style.color = "#000";
+})
+
+$changeTimeMonthly.addEventListener("click" , () => {
+    $cardPriceTime1.textContent = "month";
+    $cardPriceTime2.textContent = "month";
+    $cardPriceTime3.textContent = "month";
+
+    $cardPricePro.textContent = "$15";
+    $cardPriceEnterprise.textContent = "$99";
+
+    $changeTimeMonthly.style.backgroundColor = "#4927ec";
+    $changeTimeMonthly.style.color = "#fff";
+    $changeTimeYearly.style.backgroundColor = "transparent";
+    $changeTimeYearly.style.color = "#000";
+
+})
+
